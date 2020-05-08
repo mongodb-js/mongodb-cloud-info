a671b91cc137ffc3d189205fb3dc925e531a2071
Alena Khineika <alena.khineika@gmail.com>
2020-05-08 14:20:59 +0200

    feat: get cloud info

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # mongodb-cloud-info
 Get cloud information based on hostname
+
+## The return object format
+
+```
+{
+  isAws: false,
+  isGcp: false,
+  isAzure: false
+}
+```
---
+++ test/get-cloud-info.spec.js
@@ -0,0 +1,62 @@
+const getCloudInfo = require('../.').getCloudInfo;
+const chai = require('chai');
+
+const expect = chai.expect;
+
+describe('getCloudInfo', () => {
+  it('returns all false for undefined', async() => {
+    const cloudInfo = await getCloudInfo();
+    expect(cloudInfo).to.deep.equal({
+      is_aws: false,
+      is_gcp: false,
+      is_azure: false
+    });
+  });
+
+
+  it('returns all false for localhost', async() => {
+    const cloudInfo = await getCloudInfo('localhost');
+    expect(cloudInfo).to.deep.equal({
+      is_aws: false,
+      is_gcp: false,
+      is_azure: false
+    });
+  });
+
+  it('works with local ip address (127.0.0.1)', async() => {
+    const cloudInfo = await getCloudInfo('127.0.0.1');
+    expect(cloudInfo).to.deep.equal({
+      is_aws: false,
+      is_gcp: false,
+      is_azure: false
+    });
+  });
+
+  it('returns {is_aws: true} if hostname is an AWS ip', async() => {
+    const cloudInfo = await getCloudInfo('13.248.118.1');
+    expect(cloudInfo).to.deep.equal({
+      is_aws: true,
+      is_gcp: false,
+      is_azure: false
+    });
+  });
+
+  it('returns {is_gcp: true} if hostname is a GCP ip', async() => {
+    const cloudInfo = await getCloudInfo('8.34.208.1');
+    expect(cloudInfo).to.deep.equal({
+      is_aws: false,
+      is_gcp: true,
+      is_azure: false
+    });
+  });
+
+  it('returns {is_azure: true} if hostname is an Azure ip', async() => {
+    const cloudInfo = await getCloudInfo('13.64.151.161');
+    expect(cloudInfo).to.deep.equal({
+      is_aws: false,
+      is_gcp: false,
+      is_azure: true
+    });
+  });
+});
+
